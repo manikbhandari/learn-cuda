6ed642edbf9552352427a38765efa489705da505
manikbhandari <mbbhandarimanik2@gmail.com>
2024-01-02 05:43:11 -0500

    fix tab size

--- hello.cu
+++ hello.cu
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 __global__ void hello(){
-
-  printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
+    printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
 }
 
 int main(){
-
-  hello<<<2, 2>>>();
-  cudaDeviceSynchronize();
+    hello<<<2, 2>>>();
+    cudaDeviceSynchronize();
 }
