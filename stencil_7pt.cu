185897ca28e47d47c07bede7797953ca222c699e
manikbhandari <mbbhandarimanik2@gmail.com>
2024-08-13 08:48:52 -0700

    Adds cpu stencil 7pt

--- stencil_7pt.cu
+++ stencil_7pt.cu
@@ -8,85 +8,108 @@
 
 #define BLOCK_SZ 32
 #define DEBUG true
+#define C0 1
+#define C1 1
 
 using namespace std;
 
-const float* getRandomMatrix(unsigned int numRows, unsigned int numCols) {
-    unsigned int numElements = numRows * numCols;
-    float* matrix = new float[numRows * numCols];
+const float* getRandomMatrix(unsigned int X1, unsigned int X2, unsigned int X3) {
+    unsigned int numElements = X1 * X2 * X3;
+    float* matrix = new float[numElements];
     for(unsigned int i = 0; i < numElements; i++) {
-        matrix[i] = rand() / (float)RAND_MAX;
+        // matrix[i] = rand() / (float)RAND_MAX;
+        matrix[i] = 1.0;
     }
     return (const float*)matrix;
 }
 
-void printMatrix(const float* matrix, unsigned int numRows, unsigned int numCols) {
+void printMatrix(const float* matrix, unsigned int X1, unsigned int X2, unsigned int X3) {
     if(matrix == nullptr) {
         cout << "Cannot print null matrix";
         return;
     }
-    for (unsigned int i = 0; i < numRows; i++) {
-        for (unsigned int j = 0; j < numCols; j++)
-        {
-            cout << int(matrix[i * numCols + j]) << " ";
+    for (unsigned int i = 0; i < X3; i++) {
+        for (unsigned int j = 0; j < X2; j++) {
+            for (unsigned int k = 0; k < X1; k++) {
+                cout << int(matrix[i * X1 * X2 + j * X2 + k]) << " ";
+            }
+            cout << endl;
         }
         cout << endl;
     }
 }
 
-float* cpu_stencil_7pt(const float* matrix_1, unsigned int numRows_1, unsigned int numCols_1) {
+float* cpu_stencil_7pt(const float* matrix_1, unsigned int X1, unsigned int X2, unsigned int X3) {
+    float* ans = new float[X1 * X2 * X3];
+    for (unsigned int i = 0; i < X3; i++) {
+        for (unsigned int j = 0; j < X2; j++) {
+            for (unsigned int k = 0; k < X1; k++) {
+                ans[i * X1 * X2 + j * X2 + k] = C0 * matrix_1[i * X1 * X2 + j * X2 + k] 
+                                           + C1 * (
+                                                (k == X1 - 1 ? 0 : matrix_1[i * X1 * X2 + j * X2 + k + 1])
+                                                + (k == 0 ? 0 : matrix_1[i * X1 * X2 + j * X2 + k - 1])
+                                                + (j == X2 - 1 ? 0 : matrix_1[i * X1 * X2 + (j + 1) * X2 + k])
+                                                + (j == 0 ? 0 : matrix_1[i * X1 * X2 + (j - 1) * X2 + k])
+                                                + (i == X3 - 1 ? 0 : matrix_1[(i + 1) * X1 * X2 + j * X2 + k])
+                                                + (i == 0 ? 0 : matrix_1[(i - 1) * X1 * X2 + j * X2 + k])
+                                            );
+            }
+        }
+    }
+    return ans;
 }
 
-__global__ void gpu_stencil_7pt(float* matrix_1_d, unsigned int numRows_1, unsigned int numCols_1,
+__global__ void gpu_stencil_7pt(float* matrix_1_d, unsigned int X1, unsigned int X2, unsigned int X3,
                                 float* matrix_ans_d) {
 }
 
 int main() {
-    unsigned int numRows_1 = DEBUG ? 5 : 1024;
-    unsigned int numCols_1 = DEBUG ? 5: 1024;
-    const float* matrix_1 = getRandomMatrix(numRows_1, numCols_1);
+    unsigned int X1 = DEBUG ? 3 : 1024;
+    unsigned int X2 = DEBUG ? 3: 1024;
+    unsigned int X3 = DEBUG ? 3: 1024;
+    const float* matrix_1 = getRandomMatrix(X1, X2, X3);
 
     if(DEBUG) {
         cout << "Matrix 1:" << endl;
-        printMatrix(matrix_1, numRows_1, numCols_1);
+        printMatrix(matrix_1, X1, X2, X3);
     }
     clock_t t0 = clock();
-    float* matrix_ans = cpu_stencil_7pt(matrix_1, numRows_1, numCols_1);
+    float* matrix_ans = cpu_stencil_7pt(matrix_1, X1, X2, X3);
     clock_t t1 = clock();
     float cpu_time = ((double)(t1 - t0)) / CLOCKS_PER_SEC;
     printf("CPU stencil took %fs\n", cpu_time);
     if(DEBUG) {
         cout << "Matrix Ans:" << endl;
-        printMatrix(matrix_ans, numRows_1, numCols_1);
+        printMatrix(matrix_ans, X1, X2, X3);
     }
 
     clock_t t_gpu_overall_st = clock();
     // allocate memory on device
     float* matrix_1_d;
     float* matrix_ans_d;
-    cudaMalloc(&matrix_1_d, numRows_1 * numCols_1 * sizeof(float));
-    cudaMalloc(&matrix_ans_d, numRows_1 * numCols_1 * sizeof(float));
+    cudaMalloc(&matrix_1_d, X1 * X2 * X3 * sizeof(float));
+    cudaMalloc(&matrix_ans_d, X1 * X2 * X3 * sizeof(float));
     cudaDeviceSynchronize();
     
     // copy to device
-    cudaMemcpy(matrix_1_d, matrix_1, numRows_1 * numCols_1 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(matrix_1_d, matrix_1, X1 * X2 * X3 * sizeof(float), cudaMemcpyHostToDevice);
     cudaDeviceSynchronize();
 
     // compute
     clock_t t2 = clock();
     dim3 numThreadsPerBlock(BLOCK_SZ, BLOCK_SZ);  // 1024 is the max allowed value for this
-    int numBlocksPerThread_x = (numCols_1 + BLOCK_SZ - 1) / BLOCK_SZ;
-    int numBlocksPerThread_y = (numRows_1 + BLOCK_SZ - 1) / BLOCK_SZ;
+    int numBlocksPerThread_x = (max(max(X1, X2), X3) + BLOCK_SZ - 1) / BLOCK_SZ;
+    int numBlocksPerThread_y = (max(max(X1, X2), X3) + BLOCK_SZ - 1) / BLOCK_SZ;
     if(DEBUG) {
         printf("numBlocksPerThread_x=%d, numBlocksPerThread_y=%d\n", numBlocksPerThread_x, numBlocksPerThread_y);
     }
     dim3 numBlocksPerThread(numBlocksPerThread_x, numBlocksPerThread_y);
-    gpu_stencil_7pt <<< numBlocksPerThread, numThreadsPerBlock >>> (matrix_1_d, numRows_1, numCols_1, matrix_ans_d);
+    gpu_stencil_7pt <<< numBlocksPerThread, numThreadsPerBlock >>> (matrix_1_d, X1, X2, X3, matrix_ans_d);
     cudaDeviceSynchronize();
 
     // copy result to host
-    float* gpu_matrix_ans = new float[numRows_1 * numCols_1];
-    cudaMemcpy(gpu_matrix_ans, matrix_ans_d, numRows_1 * numCols_1 * sizeof(float), cudaMemcpyDeviceToHost);
+    float* gpu_matrix_ans = new float[X1 * X2 * X3];
+    cudaMemcpy(gpu_matrix_ans, matrix_ans_d, X1 * X2 * X3 * sizeof(float), cudaMemcpyDeviceToHost);
     cudaDeviceSynchronize();
     clock_t t3 = clock();
     float gpu_time = ((double)(t3 - t2)) / CLOCKS_PER_SEC;
@@ -102,14 +125,16 @@ int main() {
 
     // Check correctness
     float eps = 1e-2;
-    for(int i = 0; i < numRows_1; i++) {
-        for(int j = 0; j < numCols_1; j++) {
-            float cpu_ans = matrix_ans[i * numCols_1 + j];
-            float gpu_ans = gpu_matrix_ans[i * numCols_1 + j];
-            if (fabs(cpu_ans - gpu_ans) >= eps) {
-                printf("At position (%d, %d) matrix_ans has %f but gpu_matrix_ans has %f\n", i, j, cpu_ans, gpu_ans);
-                printf("Fatal error. Returning early.\n");
-                return 0;
+    for(int i = 0; i < X3; i++) {
+        for(int j = 0; j < X2; j++) {
+            for(int k = 0; k < X1; k++) {
+                float cpu_ans = matrix_ans[i * X3 + j*X2 + k];
+                float gpu_ans = gpu_matrix_ans[i * X3 + j*X2 + k];
+                if (fabs(cpu_ans - gpu_ans) >= eps) {
+                    printf("At position (%d, %d) matrix_ans has %f but gpu_matrix_ans has %f\n", i, j, cpu_ans, gpu_ans);
+                    printf("Fatal error. Returning early.\n");
+                    return 0;
+                }
             }
         }
     }
